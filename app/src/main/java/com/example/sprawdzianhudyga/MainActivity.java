38491a9e61a365e75304f0c72b3e8aa110450415
Karolina <khudyga@gmail.com>
2025-10-22 13:16:11 +0200

    dodanie TextView w Javie

--- app/src/main/java/com/example/sprawdzianhudyga/MainActivity.java
+++ app/src/main/java/com/example/sprawdzianhudyga/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sprawdzianhudyga;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView t1 =  findViewById(R.id.tekst);
     }
 }
